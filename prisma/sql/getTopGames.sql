0567eea360c93bbed9b5635349965bad87d7e39f
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2025-10-19 15:51:23 +0200

    Update sql query to get top10 games

--- prisma/sql/getTopGames.sql
+++ prisma/sql/getTopGames.sql
@@ -1,4 +1,5 @@
 select *, EXTRACT(epoch from (g."endDate" - g."startDate")) as length from "Game" g 
 where g."endDate" is not null
+and g."player" is not null
 order by (g."endDate" - g."startDate") asc
 limit 10;
